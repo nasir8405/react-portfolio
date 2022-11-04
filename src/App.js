d30e2ba968713f6f1d05261c3b4d82b1ab27c622
Nasir Khan <nasirkhan000@gmail.com>
2022-11-05 03:45:12 +0530

    Code Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { About } from "./Components/About/About";
 import { Contact } from "./Components/Contact/Contact";
 import { Experience } from "./Components/Experience/Experience";
-import { Footer } from "./Components/Footer/Footer";
 import { Header } from "./Components/Header/Header";
 import { Nav } from "./Components/Nav/Nav";
 import { Portfolio } from "./Components/Portfolio/Portfolio";
@@ -9,13 +8,12 @@ import { Portfolio } from "./Components/Portfolio/Portfolio";
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Header/>
-      <About/>
-      <Experience/>
-      <Portfolio/>
-      <Contact/>
-      <Footer/>
+      <Nav />
+      <Header />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
     </div>
   );
 }
