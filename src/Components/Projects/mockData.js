c144ae15aa9ddbe68e3240b729c873eb101aedf5
Nasir Khan <nasirkhan000@gmail.com>
2022-11-05 08:21:01 +0530

    New Code Updated

--- src/Components/Projects/mockData.js
+++ src/Components/Projects/mockData.js
@@ -8,4 +8,11 @@ export const projectData = [
     githubCodeUrl: "https://github.com/nasir8405/my-portfolio",
     demoUrl: "https://portfolio-nasir-khan.netlify.app/",
   },
+  {
+    id: 2,
+    image: project1,
+    title: "My Portfolio",
+    githubCodeUrl: "https://github.com/nasir8405/my-portfolio",
+    demoUrl: "https://portfolio-nasir-khan.netlify.app/",
+  },
 ];
