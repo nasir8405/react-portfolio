1ee6cc047dfc22e82161ca3bc6a07d36ae3b8739
Nasir Khan <nasirkhan000@gmail.com>
2022-11-05 08:51:16 +0530

    New Code Updated

--- src/Components/Projects/mockData.js
+++ src/Components/Projects/mockData.js
@@ -1,4 +1,5 @@
 import project1 from "../../Assets/project1.PNG";
+import project2 from "../../Assets/project2.PNG";
 
 export const projectData = [
   {
@@ -7,5 +8,12 @@ export const projectData = [
     title: "My Portfolio",
     githubCodeUrl: "https://github.com/nasir8405/my-portfolio",
     demoUrl: "https://portfolio-nasir-khan.netlify.app/",
+  },
+  {
+    id: 2,
+    image: project2,
+    title: "CRUD App",
+    githubCodeUrl: "https://github.com/nasir8405/crud-operation-context-api",
+    demoUrl: "https://nasir-khan-crud-app.netlify.app/",
   },
 ];
