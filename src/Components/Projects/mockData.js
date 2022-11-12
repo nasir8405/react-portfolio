aec709bf8a778ede9c6007ca30f93651534bb22d
Nasir Khan <nasirkhan000@gmail.com>
2022-11-12 20:05:47 +0530

    Code Updated

--- src/Components/Projects/mockData.js
+++ src/Components/Projects/mockData.js
@@ -1,19 +1,35 @@
-import project1 from "../../Assets/project1.PNG";
-import project2 from "../../Assets/project2.PNG";
+import portfolio from "../../Assets/portfolio.PNG";
+import crudApp from "../../Assets/crudApp.PNG";
+import rentPayment from "../../Assets/rentPayment.PNG";
+import project4 from "../../Assets/project4.PNG";
 
 export const projectData = [
   {
     id: 1,
-    image: project1,
+    image: portfolio,
     title: "My Portfolio",
     githubCodeUrl: "https://github.com/nasir8405/my-portfolio",
     demoUrl: "https://portfolio-nasir-khan.netlify.app/",
   },
   {
     id: 2,
-    image: project2,
+    image: rentPayment,
+    title: "Rent Payment",
+    githubCodeUrl: "https://github.com/nasir8405",
+    demoUrl: "https://rentpayment.in/",
+  },
+  {
+    id: 3,
+    image: project4,
+    title: "E-commerce",
+    githubCodeUrl: "https://github.com/nasir8405/e-commerce-website",
+    demoUrl: "https://electronic-commerce-website.netlify.app/",
+  },
+  {
+    id: 4,
+    image: crudApp,
     title: "CRUD App",
-    githubCodeUrl: "https://github.com/nasir8405/crud-operation-context-api",
+    githubCodeUrl: "https://github.com/nasir8405/crud-operation-redux",
     demoUrl: "https://nasir-khan-crud-app.netlify.app/",
   },
 ];
