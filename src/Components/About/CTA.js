4c511ded4c64dd54ef994a9c48d416fafb76a3cf
Nasir Khan <70660032+nasirkhan000k@users.noreply.github.com>
2025-02-12 13:11:58 +0530

    Update CTA.js

--- src/Components/About/CTA.js
+++ src/Components/About/CTA.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import CV from "../../Assets/cv.pdf"
+import newCV from "../../Assets/CV.pdf"
 
 export const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} className="btn">Download CV</a>
+        <a href={newCV} className="btn">Download CV</a>
         <a href="#contact" className="btn btn-primary">Contact Me</a>
     </div>
   )
